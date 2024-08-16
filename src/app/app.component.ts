7a76493e6a5e4651d10d088706e6b12c3dc7d04f
Navneet <hunnyhunnyhunny@gmail.com>
2024-08-16 09:33:35 -0400

    added search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,14 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CoursesService } from './courses.service';
 import getSymbolFromCurrency from 'currency-symbol-map'
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatTableModule, MatPaginatorModule],
+  imports: [RouterOutlet, MatTableModule, MatPaginatorModule, MatFormFieldModule, MatInputModule, MatTooltipModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -24,7 +27,10 @@ export class AppComponent implements OnInit, AfterViewInit {
   constructor(private coursesService: CoursesService) {
 
   }
-
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
   getDifferenceinDays(startDate: string, endDate: string) {
     const start: Date = new Date(startDate);
@@ -52,7 +58,7 @@ export class AppComponent implements OnInit, AfterViewInit {
         Location: `${course.City}, ${course.Country}, ${course.University}`,
         Start: course.StartDate,
         Length: this.getDifferenceinDays(course.StartDate, course.EndDate),
-        Price: '' + getSymbolFromCurrency(course.Currency) + Math.round(course.Price)
+        Price: '' + getSymbolFromCurrency(course.Currency) + Math.round(course.Price).toLocaleString()
       }));
 
       this.dataSource = new MatTableDataSource(updatedCourses);
